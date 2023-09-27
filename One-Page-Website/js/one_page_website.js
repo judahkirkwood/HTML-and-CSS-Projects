d9a8364419d7861702e9a75731e19955608a1651
judahkirkwood <judahkirkwood@gmail.com>
2023-09-27 10:23:54 -0700

    update
    update

--- One-Page-Website/js/one_page_website.js
+++ One-Page-Website/js/one_page_website.js
@@ -1,23 +1,40 @@
-// Lightbox open
-function openLightbox(imageUrl) {
-    var lightboxModal = document.getElementById('lightbox-modal');
-    var lightboxImage = document.getElementById('lightbox-image');
-    lightboxImage.src = imageUrl;
-    lightboxModal.style.display = 'block';
-}
-
-// Lightbox close
-function closeLightbox() {
-    var lightboxModal = document.getElementById('lightbox-modal');
-    lightboxModal.style.display = 'none';
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    var photoOverlays = document.querySelectorAll('.photo-overlay');
-    for (var i = 0; i < photoOverlays.length; i++) {
-        photoOverlays[i].addEventListener('click', function (event) {
-            var imageUrl = event.target.previousElementSibling.src;
-            openLightbox(imageUrl);
-        });
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
     }
-});
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+  }
